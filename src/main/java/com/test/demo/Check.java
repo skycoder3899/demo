23478564b7b2873d1891de8cf67430b743b799c7
Prateek Kesharwani <kesharwani3899@gmail.com>
2025-02-03 14:42:08 +0530

    new

---
+++ src/main/java/com/test/demo/Check.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Check {
+    @GetMapping()
+    public String getGreeting() {
+        return "Hi, Prateek Kesharwani";
+    }
+}
